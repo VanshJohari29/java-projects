021c773c5550f83b1030ec00e6b78d9a021e59e0
VanshJohari29 <vansh2039@gmail.com>
2026-04-05 19:47:43 +0530

    Exception handling practice

---
+++ src/ExceptionPractice.java
@@ -0,0 +1,22 @@
+public class ExceptionPractice {
+    public static void main(String[] args){
+        int result = 10;
+        try {
+            int[] arr = new int[3];
+            arr[5] = 10;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic exception found");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array Index out of bound exception");
+        }
+        catch(Exception e){
+            System.out.println("Any other excetion");
+        }
+        finally {
+            System.out.println(result);
+        }
+
+    }
+}
