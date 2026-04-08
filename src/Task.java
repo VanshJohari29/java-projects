db660ffdc10ea1fe4a71eaf3ca79dfcaa561f056
VanshJohari29 <vansh2039@gmail.com>
2026-04-08 23:40:58 +0530

    updated the task file with properties

---
+++ src/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private int id;
+    private String title;
+    private String category;
+    private String priority;
+    private String status;
+    private String createdDate;
+    public Task(int id , String title ,String category , String priority , String status , String createdDate){
+        this.id = id;
+        this.category = category;
+        this.priority = priority;
+        this.title = title;
+        this.status = status;
+        this.createdDate = createdDate;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public String getPriority(){
+        return priority;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public String getCreatedDate(){
+        return  createdDate;
+    }
+}
